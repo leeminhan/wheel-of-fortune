db21348adbd4852bd08ec40aa29374a3a7a6d3b4
bobbylin <1945775-bob-sgdev@users.noreply.gitlab.com>
2022-05-13 16:33:35 +0800

    Add tests for SpinWheel.sol

--- test/spinwheel-test.js
+++ test/spinwheel-test.js
@@ -13,5 +13,25 @@ describe("SpinWheel", function () {
     spinWheelContract = await SpinWheel.deploy();
     await spinWheelContract.deployed();
   });
-  
+
+  it("Should return gameId and topic after start game", async function () {
+    let status = await spinWheelContract.connect(owner).startGame();
+    let startGame = await status.wait();
+
+    expect(startGame.events[0].args["topic"].length).to.above(0);
+    expect(parseInt(startGame.events[0].args["gameId"])).to.equal(0);
+
+    status = await spinWheelContract.connect(addr1).startGame();
+    startGame = await status.wait();
+
+    expect(startGame.events[0].args["topic"].length).to.above(0);
+    expect(parseInt(startGame.events[0].args["gameId"])).to.equal(1);
+  });
+
+  it("Should return GameStatus after passing gameId to getGameStatus", async function() {
+    let status = await spinWheelContract.connect(owner).getGameStatus(0);
+    console.log(status["_address"]);
+    expect(status["_address"]).to.equal(owner.address);
+  })
+
 });
